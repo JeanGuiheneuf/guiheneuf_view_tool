c9ddb2660c7fbdda283b6ffdb522ef6cc38404ea
Kaydrax <jguiheneuf44@gmail.com>
2018-10-07 14:18:17 +0200

    Bug correction

--- lib/guiheneuf_view_tool/renderer.rb
+++ lib/guiheneuf_view_tool/renderer.rb
@@ -1,5 +1,7 @@
-class Renderer
+module GuiheneufViewTool
+	class Renderer
 		def self.copyright name, msg
 			"<p><b>&copy; #{Time.now.year}</b> | <b>#{name}</b> #{msg}.</p>".html_safe
 		end
+	end
 end
